75bd2743ced99baafd5b7b1ae7c6c833370c8c77
Dénes Berger <dberger@cloudera.com>
2024-01-24 13:30:50 +0100

    Initial commit

---
+++ src/main/java/org/example/repro/ReproApplication.java
@@ -0,0 +1,13 @@
+package org.example.repro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReproApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/repro/TestController.java
@@ -0,0 +1,13 @@
+package org.example.repro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(path = "/test")
+    public String testResponse() {
+        return "testing";
+    }
+}
